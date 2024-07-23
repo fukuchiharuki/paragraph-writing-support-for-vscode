2c7e36df922b19902a46bb2ab6bff4ea3cf5c5a2
fukuchiharuki <fukuchi.haruki@gmail.com>
2024-07-23 19:24:49 +0900

    fix: 不要ファイル

--- src/presentation/debounce.ts
+++
@@ -1,9 +0,0 @@
-let updateTimer: NodeJS.Timeout | null = null;
-
-export default function debounce(fn: () => void, ms: number = 500) {
-	if (updateTimer) {
-		clearTimeout(updateTimer);
-		updateTimer = null;
-	}
-	updateTimer = setTimeout(fn, ms);
-}
